c6ed33ef88252c941d01e5d5674f3b2158d08e2c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-05 00:27:16 +0530

    refactor container

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,33 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container( // app body
-          decoration: const BoxDecoration( // BoxDecoration to style the container
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Color.fromARGB(255, 124, 73, 243), Colors.blue],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello world!!!", // Text to display
-              style: TextStyle( // TextStyle to style the text
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget{
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration( // BoxDecoration to style the container
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Color.fromARGB(255, 124, 73, 243), Colors.blue],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello world!!!", // Text to display
+          style: TextStyle( // TextStyle to style the text
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
 }
