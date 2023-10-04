eddf3db290d18d159961fb8ded8e55cbf80b535a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-05 01:05:15 +0530

    update variable types

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -10,12 +10,12 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         // BoxDecoration to style the container
         gradient: LinearGradient(
           begin: startAlignment,
           end: endAlignment,
-          colors: const [
+          colors:  [
             Color.fromARGB(255, 124, 73, 243),
             Colors.blue,
           ],
