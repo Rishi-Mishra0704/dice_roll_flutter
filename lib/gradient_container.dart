1ad0229793487d578479e44633a395a98395c1c6
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-05 01:02:23 +0530

    use variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         // BoxDecoration to style the container
         gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Color.fromARGB(255, 124, 73, 243), Colors.blue],
+          begin: startAlignment,
+          end: endAlignment,
+          colors: const [
+            Color.fromARGB(255, 124, 73, 243),
+            Colors.blue,
+          ],
         ),
       ),
       child: const Center(
